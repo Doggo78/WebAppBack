cfac220432bdb4822eae329eed6bf7a8bd55f756
Mv1cc <mario.villena16@gmail.com>
2024-12-13 02:41:10 -0300

    prueba

--- webapp-backend/server.js
+++ webapp-backend/server.js
@@ -80,5 +80,5 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
